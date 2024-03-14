2c5608cb6a39f7adfc7f2576ad8aff1c4890d08c
Jürgen Seitz <juergen.seitz@h-d-gmbh.de>
2024-03-14 12:22:44 +0100

    build: prepare release v0.0.1

--- release.sh
+++ release.sh
@@ -10,16 +10,17 @@ git push
 # git tag v$1
 # git push -u origin v$1
 
-cargo publish -p qm-entity
-cargo publish -p qm-entity-derive
-cargo publish -p qm-customer
-cargo publish -p qm-server
-cargo publish -p qm-mongodb
+cargo publish -p qm-utils-derive
+cargo publish -p qm-utils
 cargo publish -p qm-redis
+cargo publish -p qm-mongodb
 cargo publish -p qm-s3
 cargo publish -p qm-kafka
 cargo publish -p qm-keycloak
-cargo publish -p qm-role
 cargo publish -p qm-role-build
-cargo publish -p qm-utils
+cargo publish -p qm-role
+cargo publish -p qm-entity-derive
+cargo publish -p qm-entity
+cargo publish -p qm-customer
+cargo publish -p qm-server
 cargo publish -p qm
