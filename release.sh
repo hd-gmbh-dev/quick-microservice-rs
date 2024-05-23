34f42f6a55ade6395f4b03fbfec9abc1aef755d0
Alexander Korolev <alexander.korolev@h-d-gmbh.de>
2024-05-23 16:03:22 +0200

    publish using actions (#16)
    * publish using actions
    
    * added rust clippy component

--- release.sh
+++ release.sh
@@ -9,21 +9,5 @@ git add .
 git commit -m "build: prepare release v$1"
 git push
 
-# git tag v$1
-# git push -u origin v$1
-
-cargo publish -p qm-utils-derive --allow-dirty
-cargo publish -p qm-utils --allow-dirty
-cargo publish -p qm-role-build --allow-dirty
-cargo publish -p qm-role --allow-dirty
-cargo publish -p qm-pg --allow-dirty
-cargo publish -p qm-redis --allow-dirty
-cargo publish -p qm-mongodb --allow-dirty
-cargo publish -p qm-s3 --allow-dirty
-cargo publish -p qm-kafka --allow-dirty
-cargo publish -p qm-keycloak --allow-dirty
-cargo publish -p qm-entity-derive --allow-dirty
-cargo publish -p qm-entity --allow-dirty
-cargo publish -p qm-customer --allow-dirty
-cargo publish -p qm-server --allow-dirty
-cargo publish -p qm --allow-dirty
+git tag v$1
+git push --tag
