7b7cfbb6eacfff85cb3b94937e28420ff1f60d1a
Seitz Jürgen - H & D <seitz.h-d@partner.akdb.de>
2025-01-23 06:58:19 +0100

    added nats

---
+++ crates/nats/src/communal.rs
@@ -0,0 +1,370 @@
+use sea_orm::EntityName;
+use strum::AsRefStr;
+use async_graphql::Enum;
+
+#[derive(Default, AsRefStr, Clone, Copy, PartialEq, Eq, Enum)]
+#[strum(serialize_all = "lowercase")]
+pub enum Op {
+    #[default]
+    Req,
+    Res,
+}
+
+#[derive(Default, AsRefStr, Clone, Copy, PartialEq, Eq, Enum)]
+#[strum(serialize_all = "lowercase")]
+pub enum Type {
+    #[default]
+    Unknown,
+    Create,
+    Update,
+    Delete,
+    Download,
+    Upload,
+    Import,
+    Export,
+    Send,
+    Recv,
+    Assign,
+    Unassign,
+}
+
+#[derive(Default, AsRefStr, Clone, Copy, PartialEq, Eq, Enum)]
+#[strum(serialize_all = "UPPERCASE")]
+pub enum ParentCtx {
+    #[default]
+    DE,
+    BW,
+    BY,
+    BE,
+    BB,
+    HB,
+    HH,
+    HE,
+    MV,
+    NI,
+    NW,
+    RP,
+    SL,
+    SN,
+    ST,
+    SH,
+    TH,
+}
+
+impl From<i16> for ParentCtx {
+    fn from(value: i16) -> Self {
+        match value {
+            0x08 => ParentCtx::BW,
+            0x09 => ParentCtx::BY,
+            0x0B => ParentCtx::BE,
+            0x0C => ParentCtx::BB,
+            0x04 => ParentCtx::HB,
+            0x02 => ParentCtx::HH,
+            0x06 => ParentCtx::HE,
+            0x0D => ParentCtx::MV,
+            0x03 => ParentCtx::NI,
+            0x05 => ParentCtx::NW,
+            0x07 => ParentCtx::RP,
+            0x0A => ParentCtx::SL,
+            0x0E => ParentCtx::SN,
+            0x0F => ParentCtx::ST,
+            0x01 => ParentCtx::SH,
+            0x10 => ParentCtx::TH,
+            _ => ParentCtx::DE,
+        }
+    }
+}
+
+impl From<Option<i16>> for ParentCtx {
+    fn from(value: Option<i16>) -> Self {
+        let value = value.unwrap_or(0);
+        ParentCtx::from(value)
+    }
+}
+
+#[derive(Default, AsRefStr, Clone, Copy, PartialEq, Eq, Enum)]
+#[strum(serialize_all = "UPPERCASE")]
+pub enum CtxType {
+    #[default]
+    System,
+    Olb,
+    OlbBranch,
+    Hdp,
+    Wsp,
+    WspBranch,
+    Lab,
+    LabBranch,
+}
+
+#[derive(Default)]
+struct Event {
+    op: Op,
+    ty: Type,
+    parent_ctx: ParentCtx,
+    ctx_type: CtxType,
+    ctx: Option<String>,
+    request_id: Option<String>,
+    actor: Option<String>,
+    error: bool,
+}
+
+fn format(ev: &Event, resource_name: impl std::fmt::Display) -> String {
+    let append = match ev.op {
+        Op::Req => "",
+        Op::Res => {
+            if ev.error {
+                ".error"
+            } else {
+                ".success"
+            }
+        }
+    };
+    format!(
+        "ev.{}.{}.{}.{}.{}.{}.{}.{}{append}",
+        ev.op.as_ref(),
+        ev.parent_ctx.as_ref(),
+        ev.ctx_type.as_ref(),
+        ev.ctx.as_deref().unwrap_or("none"),
+        resource_name,
+        ev.ty.as_ref(),
+        ev.request_id.as_deref().unwrap_or("none"),
+        ev.actor.as_deref().unwrap_or("none"),
+    )
+}
+pub trait ResourceName {
+    fn name(&self) -> &str;
+}
+
+pub trait StaticResourceName {
+    fn name() -> &'static str;
+}
+
+pub struct Subject<E>(Event, E);
+impl<E> Subject<E>
+where
+    E: Default,
+{
+    pub fn op(&self) -> Op {
+        self.0.op
+    }
+
+    pub fn ty(&self) -> Type {
+        self.0.ty
+    }
+
+    pub fn parent_ctx(&self) -> ParentCtx {
+        self.0.parent_ctx
+    }
+
+    pub fn ctx_type(&self) -> CtxType {
+        self.0.ctx_type
+    }
+
+    pub fn ctx(&self) -> Option<&str> {
+        self.0.ctx.as_deref()
+    }
+
+    pub fn request_id(&self) -> Option<&str> {
+        self.0.request_id.as_deref()
+    }
+
+    pub fn actor(&self) -> Option<&str> {
+        self.0.actor.as_deref()
+    }
+
+    pub fn error(&self) -> bool {
+        self.0.error
+    }
+
+    fn factory(ty: Type) -> Self {
+        let mut ev = Event::default();
+        ev.ty = ty;
+        Self(ev, E::default())
+    }
+
+    pub fn create() -> Self {
+        Self::factory(Type::Create)
+    }
+
+    pub fn update() -> Self {
+        Self::factory(Type::Update)
+    }
+
+    pub fn delete() -> Self {
+        Self::factory(Type::Delete)
+    }
+
+    pub fn download() -> Self {
+        Self::factory(Type::Download)
+    }
+
+    pub fn upload() -> Self {
+        Self::factory(Type::Upload)
+    }
+
+    pub fn import() -> Self {
+        Self::factory(Type::Import)
+    }
+
+    pub fn export() -> Self {
+        Self::factory(Type::Export)
+    }
+
+    pub fn send() -> Self {
+        Self::factory(Type::Send)
+    }
+
+    pub fn recv() -> Self {
+        Self::factory(Type::Recv)
+    }
+
+    pub fn assign() -> Self {
+        Self::factory(Type::Assign)
+    }
+
+    pub fn unassign() -> Self {
+        Self::factory(Type::Unassign)
+    }
+
+    pub fn into_response(mut self) -> Self {
+        self.0.op = Op::Res;
+        self
+    }
+
+    pub fn into_success(mut self) -> Self {
+        self = self.into_response();
+        self.0.error = false;
+        self
+    }
+
+    pub fn into_error(mut self) -> Self {
+        self = self.into_response();
+        self.0.error = true;
+        self
+    }
+
+    pub fn with_parent_ctx<T: Into<ParentCtx>>(mut self, ctx: T) -> Self {
+        self.0.parent_ctx = ctx.into();
+        self
+    }
+
+    pub fn with_ctx_type(mut self, ctx_type: CtxType) -> Self {
+        self.0.ctx_type = ctx_type;
+        self
+    }
+
+    pub fn with_ctx<S: Into<String>>(mut self, ctx: S) -> Self {
+        self.0.ctx = Some(ctx.into());
+        self
+    }
+
+    pub fn with_request_id<S: Into<String>>(mut self, request_id: S) -> Self {
+        self.0.request_id = Some(request_id.into());
+        self
+    }
+
+    pub fn with_actor<S: Into<String>>(mut self, actor: S) -> Self {
+        self.0.actor = Some(actor.into());
+        self
+    }
+}
+
+impl<E> Subject<E>
+where
+    E: EntityName,
+{
+    pub fn entity(&self) -> async_nats::Subject {
+        format(&self.0, self.1.table_name()).into()
+    }
+}
+
+impl<E> Subject<E>
+where
+    E: ResourceName,
+{
+    pub fn resource(&self) -> async_nats::Subject {
+        format(&self.0, self.1.name()).into()
+    }
+}
+
+impl<E> Subject<std::marker::PhantomData<E>>
+where
+    E: StaticResourceName,
+{
+    pub fn static_resource(&self) -> async_nats::Subject {
+        format(&self.0, E::name()).into()
+    }
+}
+
+pub struct Entity;
+pub struct Resource;
+pub struct StaticResource;
+
+use crate::EventToSubject;
+
+impl<E> EventToSubject<Entity> for Subject<E>
+where
+    E: EntityName,
+{
+    fn event_to_subject(&self) -> async_nats::Subject {
+        self.entity()
+    }
+}
+
+impl<E> EventToSubject<Resource> for Subject<E>
+where
+    E: ResourceName,
+{
+    fn event_to_subject(&self) -> async_nats::Subject {
+        self.resource()
+    }
+}
+
+impl<E> EventToSubject<Resource> for Subject<std::marker::PhantomData<E>>
+where
+    E: StaticResourceName,
+{
+    fn event_to_subject(&self) -> async_nats::Subject {
+        self.static_resource()
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::ParentCtx;
+    use super::{ResourceName, Subject};
+
+    #[derive(Default)]
+    struct TestEntity;
+    impl ResourceName for TestEntity {
+        fn name(&self) -> &str {
+            "qr"
+        }
+    }
+
+    #[test]
+    fn test_subject_creation() {
+        let mut subject = Subject::<TestEntity>::send()
+            .with_parent_ctx(ParentCtx::BY)
+            .with_ctx_type(super::CtxType::Lab)
+            .with_ctx("R3425921760D")
+            .with_request_id("165789548978")
+            .with_actor("EA4DCDCA-1CFD-48B9-905A-60DAB47964CB");
+        assert_eq!(
+            subject.resource(),
+            async_nats::Subject::from_static("ev.req.BY.LAB.R3425921760D.qr.send.165789548978.EA4DCDCA-1CFD-48B9-905A-60DAB47964CB"),
+        );
+        subject = subject.into_success();
+        assert_eq!(
+            subject.resource(),
+            async_nats::Subject::from_static("ev.res.BY.LAB.R3425921760D.qr.send.165789548978.EA4DCDCA-1CFD-48B9-905A-60DAB47964CB.success"),
+        );
+
+        subject = subject.into_error();
+        assert_eq!(
+            subject.resource(),
+            async_nats::Subject::from_static("ev.res.BY.LAB.R3425921760D.qr.send.165789548978.EA4DCDCA-1CFD-48B9-905A-60DAB47964CB.error"),
+        );
+    }
+}
---
+++ crates/nats/src/lib.rs
@@ -0,0 +1,514 @@
+use std::{
+    collections::HashSet,
+    error::Error,
+    sync::{
+        atomic::{AtomicU64, Ordering},
+        Arc,
+    },
+};
+
+use async_nats::{
+    jetstream::{
+        self,
+        consumer::PullConsumer,
+        context::{
+            CreateKeyValueErrorKind, CreateStreamError, GetStreamErrorKind, KeyValueErrorKind,
+        },
+        kv::{self, Operation, Store},
+        stream::ConsumerError,
+        Context,
+    }, subject::ToSubject, Client, ConnectError, ConnectErrorKind
+};
+use futures::{StreamExt, TryStreamExt};
+use tokio::task::JoinHandle;
+
+pub use async_nats;
+
+pub mod communal;
+
+#[derive(Clone, serde::Deserialize)]
+pub struct Config {
+    app_name: Option<String>,
+    host: Option<String>,
+    port: Option<u16>,
+    #[serde(skip)]
+    address: Option<String>,
+    sys_locks: Option<String>,
+    events_stream_name: Option<String>,
+    events_stream_subject: Option<String>,
+}
+
+impl Config {
+    pub fn new() -> envy::Result<Self> {
+        ConfigBuilder::default().build()
+    }
+
+    pub fn builder<'a>() -> ConfigBuilder<'a> {
+        ConfigBuilder::default()
+    }
+
+    pub fn address(&self) -> &str {
+        self.address.as_deref().unwrap()
+    }
+
+    pub fn port(&self) -> u16 {
+        self.port.unwrap_or(3000)
+    }
+    pub fn sys_locks(&self) -> &str {
+        self.sys_locks.as_deref().unwrap_or("SYS_LOCKS")
+    }
+    pub fn events_stream_name(&self) -> &str {
+        self.events_stream_name.as_deref().unwrap_or("EVENTS")
+    }
+    pub fn events_stream_subject(&self) -> &str {
+        self.events_stream_subject.as_deref().unwrap_or("ev.>")
+    }
+}
+
+#[derive(Default)]
+pub struct ConfigBuilder<'a> {
+    prefix: Option<&'a str>,
+}
+
+impl<'a> ConfigBuilder<'a> {
+    pub fn with_prefix(mut self, prefix: &'a str) -> Self {
+        self.prefix = Some(prefix);
+        self
+    }
+
+    pub fn build(self) -> envy::Result<Config> {
+        let prefix = self.prefix.unwrap_or("NATS_");
+        let mut cfg: Config = envy::prefixed(prefix).from_env()?;
+        if cfg.app_name.is_none() {
+            cfg.app_name = Some("edd-service-rs".into());
+        }
+        let host = cfg.host.as_deref().unwrap_or("127.0.0.1");
+        let port = cfg.port.unwrap_or(4222);
+        cfg.address = Some(format!("nats://{}:{}", host, port));
+        Ok(cfg)
+    }
+}
+
+pub struct Inner {
+    client: Client,
+    config: Config,
+}
+
+#[derive(Clone)]
+pub struct Nats {
+    inner: Arc<Inner>,
+}
+
+impl Nats {
+    pub async fn new(config: Config) -> Result<Self, ConnectError> {
+        let client = async_nats::ConnectOptions::new()
+            .max_reconnects(Some(1))
+            .connect(config.address())
+            .await?;
+        Ok(Self {
+            inner: Arc::new(Inner { client, config }),
+        })
+    }
+
+    pub async fn publisher(&self) -> Result<Publisher, CreateStreamError> {
+        let ctx = jetstream::new(self.inner.client.clone());
+        let p = Publisher { ctx };
+        p.init(&self.inner.config).await?;
+        Ok(p)
+    }
+
+    pub async fn sys_consumer(&self, name: String) -> Result<PullConsumer, ConsumerError> {
+        let ctx = jetstream::new(self.inner.client.clone());
+        ctx.create_consumer_on_stream(
+            jetstream::consumer::pull::Config {
+                durable_name: Some(name),
+                ..Default::default()
+            },
+            self.inner.config.events_stream_name(),
+        )
+        .await
+    }
+
+    pub async fn sys_consumer_with_filter(
+        &self,
+        name: String,
+        filter_subject: String,
+    ) -> Result<PullConsumer, ConsumerError> {
+        let ctx = jetstream::new(self.inner.client.clone());
+        ctx.create_consumer_on_stream(
+            jetstream::consumer::pull::Config {
+                durable_name: Some(name),
+                filter_subject,
+                ..Default::default()
+            },
+            self.inner.config.events_stream_name(),
+        )
+        .await
+    }
+
+    pub async fn tmp_sys_consumer_with_filter(
+        &self,
+        filter_subject: String,
+    ) -> Result<PullConsumer, ConsumerError> {
+        let ctx = jetstream::new(self.inner.client.clone());
+        ctx.create_consumer_on_stream(
+            jetstream::consumer::pull::Config {
+                filter_subject,
+                deliver_policy: jetstream::consumer::DeliverPolicy::Last,
+                ..Default::default()
+            },
+            self.inner.config.events_stream_name(),
+        )
+        .await
+    }
+
+    pub async fn distributed_locks(&self) -> Result<DistributedLocks, DistributedLocksError> {
+        let ctx = jetstream::new(self.inner.client.clone());
+        DistributedLocks::new(ctx, &self.inner.config).await
+    }
+
+    pub fn sequence_manager(&self) -> SequenceManager {
+        let ctx = jetstream::new(self.inner.client.clone());
+        SequenceManager { ctx }
+    }
+}
+
+
+pub trait EventToSubject<M> {
+    fn event_to_subject(&self) -> async_nats::Subject;
+}
+
+pub struct Publisher {
+    ctx: Context,
+}
+
+impl Publisher {
+    async fn init(&self, config: &Config) -> Result<(), CreateStreamError> {
+        let names: HashSet<String> = self.ctx.stream_names().try_collect().await?;
+        if !names.contains(config.events_stream_name()) {
+            self.ctx
+                .create_stream(jetstream::stream::Config {
+                    name: config.events_stream_name().to_string(),
+                    subjects: vec![config.events_stream_subject().into()],
+                    allow_direct: true,
+                    deny_delete: true,
+                    deny_purge: true,
+                    ..Default::default()
+                })
+                .await?;
+        }
+        Ok(())
+    }
+
+    pub async fn publish<S: ToSubject, P: ?Sized + serde::Serialize>(
+        &self,
+        subject: S,
+        payload: &P,
+    ) -> anyhow::Result<()> {
+        self.ctx
+            .publish(subject, serde_json::to_vec(payload)?.into())
+            .await?;
+        Ok(())
+    }
+
+    pub async fn publish_event<S, M, P>(&self, subject: &S, payload: &P) -> anyhow::Result<()>
+    where
+        S: ?Sized + EventToSubject<M>,
+        P: ?Sized + serde::Serialize,
+    {
+        self.ctx
+            .publish(
+                subject.event_to_subject(),
+                serde_json::to_vec(payload)?.into(),
+            )
+            .await?;
+        Ok(())
+    }
+}
+
+impl AsRef<Context> for Publisher {
+    fn as_ref(&self) -> &Context {
+        &self.ctx
+    }
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum DistributedLocksError {
+    #[error(transparent)]
+    Connect(#[from] async_nats::error::Error<ConnectErrorKind>),
+    #[error(transparent)]
+    CreateKeyValue(#[from] async_nats::error::Error<CreateKeyValueErrorKind>),
+    #[error(transparent)]
+    KeyValue(#[from] async_nats::error::Error<KeyValueErrorKind>),
+}
+
+#[derive(Clone)]
+pub struct DistributedLocks {
+    ctx: Context,
+    sys_locks: String,
+}
+
+impl DistributedLocks {
+    async fn new(ctx: Context, config: &Config) -> Result<Self, DistributedLocksError> {
+        let lm = DistributedLocks { ctx, sys_locks: config.sys_locks().to_string() };
+        if !lm.exists(config.sys_locks()).await? {
+            lm.create(config.sys_locks(), 5).await?;
+        }
+        Ok(lm)
+    }
+
+    async fn create<T: Into<String>>(
+        &self,
+        name: T,
+        max_age: u64,
+    ) -> Result<Store, DistributedLocksError> {
+        Ok(self
+            .ctx
+            .create_key_value(kv::Config {
+                bucket: name.into(),
+                max_age: std::time::Duration::from_secs(max_age),
+                history: 1,
+                ..Default::default()
+            })
+            .await?)
+    }
+
+    async fn exists<T: Into<String>>(&self, bucket: T) -> Result<bool, DistributedLocksError> {
+        if let Err(err) = self.ctx.get_key_value(bucket).await {
+            if err.kind() == KeyValueErrorKind::GetBucket {
+                if let Some(src) = err.source() {
+                    let err = src.downcast_ref::<async_nats::error::Error<GetStreamErrorKind>>();
+                    if let Some(err) = err {
+                        if let GetStreamErrorKind::JetStream(err) = err.kind() {
+                            if err.code() == 404 {
+                                return Ok(false);
+                            }
+                        }
+                    }
+                }
+            }
+            Err(err)?;
+        }
+        Ok(true)
+    }
+
+    pub async fn sys_locks(&self) -> anyhow::Result<LockManager> {
+        let kv = self.ctx.get_key_value(&self.sys_locks).await?;
+        Ok(LockManager { kv: Arc::new(kv) })
+    }
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum LockManagerError {
+    #[error(transparent)]
+    CreateKeyValue(#[from] async_nats::error::Error<CreateKeyValueErrorKind>),
+    #[error(transparent)]
+    KeyValue(#[from] async_nats::error::Error<KeyValueErrorKind>),
+    #[error(transparent)]
+    Watch(#[from] async_nats::error::Error<kv::WatchErrorKind>),
+    #[error("unable to lock resource after {0:?}")]
+    OutOfRetries(std::time::Duration),
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum SequenceManagerError {
+    #[error(transparent)]
+    Connect(#[from] async_nats::error::Error<ConnectErrorKind>),
+    #[error(transparent)]
+    CreateKeyValue(#[from] async_nats::error::Error<CreateKeyValueErrorKind>),
+    #[error(transparent)]
+    KeyValue(#[from] async_nats::error::Error<KeyValueErrorKind>),
+    #[error(transparent)]
+    Put(#[from] async_nats::error::Error<async_nats::jetstream::kv::PutErrorKind>),
+    #[error(transparent)]
+    Entry(#[from] async_nats::error::Error<async_nats::jetstream::kv::EntryErrorKind>),
+}
+
+pub struct SequenceManager {
+    ctx: Context,
+}
+
+impl SequenceManager {
+    async fn create<T: Into<String>>(&self, name: T) -> Result<Store, SequenceManagerError> {
+        Ok(self
+            .ctx
+            .create_key_value(kv::Config {
+                bucket: name.into(),
+                ..Default::default()
+            })
+            .await?)
+    }
+
+    async fn exists<T: Into<String>>(&self, bucket: T) -> Result<bool, SequenceManagerError> {
+        if let Err(err) = self.ctx.get_key_value(bucket).await {
+            if err.kind() == KeyValueErrorKind::GetBucket {
+                if let Some(src) = err.source() {
+                    let err = src.downcast_ref::<async_nats::error::Error<GetStreamErrorKind>>();
+                    if let Some(err) = err {
+                        if let GetStreamErrorKind::JetStream(err) = err.kind() {
+                            if err.code() == 404 {
+                                return Ok(false);
+                            }
+                        }
+                    }
+                }
+            }
+            Err(err)?;
+        }
+        Ok(true)
+    }
+
+    async fn get<T: Into<String>>(&self, bucket: T) -> Result<Store, SequenceManagerError> {
+        Ok(self.ctx.get_key_value(bucket).await?)
+    }
+
+    pub async fn next(&self, prefix: &str, id: i64) -> Result<i64, SequenceManagerError> {
+        let bucket = format!("sm-{prefix}");
+        if !self.exists(&bucket).await? {
+            let store = self.create(&bucket).await?;
+            let result = store.put("id", id.to_be_bytes().to_vec().into()).await?;
+            Ok(result as i64)
+        } else {
+            let store = self.get(&bucket).await?;
+            let e = store.entry("id").await?;
+            if let Some(e) = e {
+                Ok(e.revision as i64)
+            } else {
+                let result = store.put("id", id.to_be_bytes().to_vec().into()).await?;
+                Ok(result as i64)
+            }
+        }
+    }
+
+    pub async fn increment(&self, prefix: &str, id: i64) -> Result<i64, SequenceManagerError> {
+        let bucket = format!("sm-{prefix}");
+        let store = self.get(&bucket).await?;
+        let e = store.put("id", id.to_be_bytes().to_vec().into()).await?;
+        Ok(e as i64)
+    }
+}
+
+pub struct LockManager {
+    kv: Arc<Store>,
+}
+
+impl LockManager {
+    pub async fn run_locked<N, O, F, E>(&self, name: N, f: F) -> Result<O, E>
+    where
+        N: Into<String>,
+        F: std::future::Future<Output = Result<O, E>>,
+        E: From<LockManagerError>,
+    {
+        let lock = self.try_lock(name.into(), 3, 5).await?;
+        let result = f.await;
+        let w_kv = self.kv.clone();
+        tokio::spawn(async move {
+            if !lock.jh.is_finished() {
+                lock.jh.abort();
+                let result = lock.jh.await;
+                if result.is_err() {
+                    let err = result.unwrap_err();
+                    if !err.is_cancelled() {
+                        tracing::error!("{err:#?}");
+                    }
+                }
+            }
+            w_kv.delete_expect_revision(lock.name, Some(lock.revision.load(Ordering::SeqCst)))
+                .await
+                .ok();
+        });
+        result
+    }
+
+    async fn try_lock(
+        &self,
+        name: String,
+        timeout: u64,
+        retries: usize,
+    ) -> Result<Lock, LockManagerError> {
+        let now = std::time::Instant::now();
+        let max_retries = retries;
+        let mut tries = 0;
+        let revision = Arc::new(AtomicU64::new(0));
+        let kv = &self.kv;
+        loop {
+            if tries >= max_retries {
+                return Err(LockManagerError::OutOfRetries(now.elapsed()));
+            }
+            let v = kv.create(&name, "r".into()).await;
+            if let Err(err) = v {
+                if err.kind() == async_nats::jetstream::kv::CreateErrorKind::AlreadyExists {
+                    tracing::info!("seems to be locked already, {tries} try to watch for changes");
+                    let mut w = kv.watch(&name).await?;
+                    let f = async {
+                        'inner: while let Some(m) = w.next().await {
+                            if let Ok(e) = m {
+                                if e.operation == Operation::Delete {
+                                    tracing::info!("retry because prev lock was deleted");
+                                    break 'inner;
+                                }
+                            }
+                        }
+                    };
+                    let t = async {
+                        tokio::time::sleep(std::time::Duration::from_secs(timeout)).await;
+                    };
+                    let change = tokio::select! {
+                        _ = f => true,
+                        _ = t => false,
+                    };
+                    if !change {
+                        tries += 1;
+                    }
+                }
+            } else {
+                let r = v.unwrap();
+                revision.store(r, Ordering::SeqCst);
+                tracing::info!("got lock");
+                break;
+            }
+        }
+        let w_kv = self.kv.clone();
+        let w_name = name.clone();
+        let w_revision = revision.clone();
+
+        let jh = tokio::spawn(async move {
+            let mut run = 0;
+            loop {
+                run += 1;
+                tokio::time::sleep(std::time::Duration::from_secs(2)).await;
+                tracing::info!("refresh lock {w_name}");
+                let result = w_kv
+                    .update(&w_name, "u".into(), w_revision.load(Ordering::SeqCst))
+                    .await;
+                if let Err(err) = result {
+                    tracing::error!("{err:#?}");
+                    break;
+                } else {
+                    w_revision.store(result.unwrap(), Ordering::SeqCst);
+                }
+                if run >= 5 {
+                    tracing::info!("release lock after timeout");
+                    break;
+                }
+            }
+            anyhow::Ok(())
+        });
+
+        Ok(Lock { name, revision, jh })
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum LockState {
+    Registering,
+    Registered,
+}
+
+#[derive(Debug)]
+pub struct Lock {
+    name: String,
+    revision: Arc<AtomicU64>,
+    jh: JoinHandle<anyhow::Result<()>>,
+}
+
